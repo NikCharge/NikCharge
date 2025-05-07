bdccc55b1bfbe2b6b386703f85ffe5a410b016af
Carolina Silva <119443000+carolinaspsilva2004@users.noreply.github.com>
2025-05-08 00:45:47 +0100

    criação do projeto

---
+++ NikCharge/src/main/java/tqs/nikcharge/NikChargeApplication.java
@@ -0,0 +1,13 @@
+package tqs.nikcharge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikChargeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NikChargeApplication.class, args);
+    }
+
+}
