df8b3c489e377bec77f2427b1bf6578dcfdaa3b8
carolinapsilva2004 <carolinaspsilva@ua.pt>
2025-05-08 16:33:21 +0100

    criei o projeto

--- Backend/src/main/java/tqs/backend/BackendApplication.java
+++ Backend/src/main/java/tqs/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package tqs.nikcharge;
+package tqs.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NikChargeApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NikChargeApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
