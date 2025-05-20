4f435bc0a36f2ec691e51984a2f8412798e9283a
angelammaribeiro <angelammaribeiro@ua.pt>
2025-05-20 16:29:37 +0100

    Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import About from "./pages/About";
 
 function App() {
-    return <Homepage />;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
