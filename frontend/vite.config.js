8dd55b328632fa2a3734052155c848584b9ce718
carolinapsilva2004 <carolinaspsilva@ua.pt>
2025-05-26 22:40:55 +0100

    temporariamente resolvido

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://backend:8080',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
