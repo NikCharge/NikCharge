93018fa3ae84235ba5a23547fc4cf088bb207af4
carolinapsilva2004 <carolinaspsilva@ua.pt>
2025-06-04 15:39:15 +0100

    correção de alguns issues do sonar

--- Backend/src/test/java/tqs/backend/stepdefs/StationFilteringStepDefs.java
+++ Backend/src/test/java/tqs/backend/stepdefs/StationFilteringStepDefs.java
@@ -13,7 +13,6 @@ import tqs.backend.repository.StationRepository;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
