0996d6bd0aa719534e26e37019f710c8c3c2bf4c
Rita-Silva04 <aritafs@ua.pt>
2025-06-05 03:49:43 +0100

    frontend corrections

--- Backend/src/test/java/tqs/backend/stepdefs/ViewCompletedReservationsStepDefs.java
+++ Backend/src/test/java/tqs/backend/stepdefs/ViewCompletedReservationsStepDefs.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import tqs.backend.util.CommonReservationHelper;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ViewCompletedReservationsStepDefs {
